6f0641f37a49699ca5e2eb3cef19ababd6063477
Farshad <13575387+0x73am43l@users.noreply.github.com>
2021-05-15 21:23:58 +0430

    Delete SoalSadeh.py

--- SoalSadeh.py
+++
@@ -1,9 +0,0 @@
-a = int(input())
-for n in ['man khoshghlab hastam\n']:
-    print(a*n)
-
-////////////////
-
-a = int(input(("Enter number: ")))
-for i in range(a):
-     print("man khoshghlab hastam")
